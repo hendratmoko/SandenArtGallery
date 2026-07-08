9d904556325f2afbeb2d16577d745efcd2c3e907
hendratmoko <128827170+hendratmoko@users.noreply.github.com>
2026-07-08 11:57:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -441,3 +441,14 @@ async function finishQuiz() {
     pendingWork = null;
   }
 }
+
+const slides=document.querySelectorAll(".slide");
+let current=0;
+setInterval(()=>{
+    slides[current].classList.remove("active");
+    current++;
+    if(current>=slides.length){
+        current=0;
+    }
+    slides[current].classList.add("active");
+},8000);
