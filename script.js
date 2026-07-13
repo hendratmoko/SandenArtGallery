495bf15cc84458e5bce6be760e7169dc816d82be
hendratmoko <128827170+hendratmoko@users.noreply.github.com>
2026-07-13 14:40:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -490,3 +490,21 @@ setInterval(()=>{
     }
     slides[current].classList.add("active");
 },8000);
+
+/*=========================
+BACK TO TOP
+=========================*/
+const backToTop = document.getElementById("backToTop");
+window.addEventListener("scroll",()=>{
+    if(window.scrollY>350){
+        backToTop.classList.add("show");
+    }else{
+        backToTop.classList.remove("show");
+    }
+});
+backToTop.addEventListener("click",()=>{
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+});
