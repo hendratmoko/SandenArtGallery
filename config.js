5aa448f7c3798282b398dbbd39d706ee90fc7ecc
hendratmoko <128827170+hendratmoko@users.noreply.github.com>
2026-08-17 12:08:41 +0700

    Update GAS_URL to new endpoint

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
  */
 window.APP_CONFIG = {
   // WAJIB DIISI — URL Web App Apps Script kamu
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbziMsv5nFrTX3GA5QDDtTAqTHkbt7r8SZzsKyPq2U9f3CP9M_9zdKUQZFzTOJ_pguoL/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzDG_T4VYVzI9mU-IDCBq4Sut29o23FokITiFdhnyamymlAbWa5k16_aNF1FeuygkT8Hw/exec',
 
   // Kalau true: saat GAS_URL belum diisi / fetch ke GAS gagal,
   // aplikasi otomatis memuat data contoh dari data.json supaya
